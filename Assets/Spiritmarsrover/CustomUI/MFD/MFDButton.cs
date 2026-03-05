fec542ab6c581364048ac3f9677b1ee30664e3f6
DESKTOP-P2AGUDS\Titan <d3coder72@gmail.com>
2026-03-04 19:20:11 -0600

    MFD worked on. Moved some scripts around.

--- Assets/Spiritmarsrover/CustomUI/MFD/MFDButton.cs
+++ Assets/Spiritmarsrover/CustomUI/MFD/MFDButton.cs
@@ -1,71 +1,183 @@
-﻿
-using UdonSharp;
+﻿using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
-using UnityEngine.EventSystems; // Required for Pointer handlers
-public enum ButtonMode
-{
-    PageChanger,
-    Event,
-    ContinousEvent,
-    None
-}
-//PageChanger changes the page
-//Event triggers an event
-//ContiousEvent runs an event every frame.
+using TMPro;
+
+public enum ButtonMode { PageChanger, Event, ContinousEvent, None }
+
 [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
 public class MFDButton : UdonSharpBehaviour
 {
+    [Header("References")]
     public MFDNavigationManager MFDManager;
+    public int ButtonID;
+
+    [Header("Visual Feedback")]
+    public Transform buttonMesh;
+    public Vector3 pressedOffset = new Vector3(0, -0.005f, 0);
+    private Vector3 _startLocalPos;
+    private bool _posInitialized = false;
+
+    [Header("Current Configuration")]
+    public ButtonMode mode = ButtonMode.None;
     public GameObject NextPage;
     public UdonBehaviour EventScript;
     public string EventName;
     public UdonBehaviour ContinousEventScript;
     public string ContinousEventName;
-    public ButtonMode mode = ButtonMode.PageChanger;
-    public int ButtonID;
+
+    public TMP_Text textToggleColor;
+    public string boolVariableName;
+
+    private int _lastPenID = -1;
+    //only useful if testing on desktop. will need to build own raycaster for desktop users. 
+    //public override void Interact()
+    //{
+    //    OnDown(0);
+    //}
+
+    //public AudioSource ButtonAudioSource;
     void Start()
     {
-
+       // ButtonAudioSource = gameObject.GetComponent<AudioSource>();
+        InitializePosition();
     }
-    public void OnButtonDown()
+
+    private void InitializePosition()
     {
-        if(mode == ButtonMode.None)
+        if (_posInitialized) return;
+        if (buttonMesh != null)
         {
-            return;
+            _startLocalPos = buttonMesh.localPosition;
+            _posInitialized = true;
         }
+    }
+
+    public void OnDown(int id,TabletPen pen)
+    {
+        if (mode == ButtonMode.None) return;
+        _lastPenID = id;
+
+        InitializePosition();
+
+        //if (Networking.LocalPlayer.IsUserInVR())
+        //{
+        //    // Move mesh DOWN
+        //    if (buttonMesh != null) buttonMesh.localPosition = _startLocalPos + pressedOffset;
+        //}
+        // Move mesh DOWN
+        if (buttonMesh != null) buttonMesh.localPosition = _startLocalPos + pressedOffset;
+
+
         if (mode == ButtonMode.PageChanger)
         {
+            //Can do Events assocated with a page change.
+            if (EventScript != null)
+            {
+                EventScript.SendCustomEvent(EventName);
+            }
             ChangePage();
+            
         }
-        else if(mode == ButtonMode.Event)
+        else if (mode == ButtonMode.Event && EventScript != null)
         {
             EventScript.SendCustomEvent(EventName);
         }
-        else if(mode == ButtonMode.ContinousEvent)
+        else if (mode == ButtonMode.ContinousEvent)
         {
             MFDManager.ContinousEventScript = ContinousEventScript;
             MFDManager.ContinousEventName = ContinousEventName;
         }
+
+        pen.PlayButtonDownClip();
+        if (EventScript == null || boolVariableName == "")
+        {
+            return;
+        }
+        updateToggleText();
+        
+
     }
-    public void OnButtonUp()
+
+    // NEW: Handle logic while the pen is held down
+    public void OnStay(int id)
     {
-        MFDManager.ContinousEventScript = null;
-        MFDManager.ContinousEventName = null;
+        if (id != _lastPenID || mode == ButtonMode.None) return;
+
+        // If for some reason the button was reset by a page change 
+        // but the pen is still held, keep the mesh DOWN.
+        if (buttonMesh != null && buttonMesh.localPosition == _startLocalPos)
+        {
+            buttonMesh.localPosition = _startLocalPos + pressedOffset;
+        }
     }
+
+    public void OnUp(int id,TabletPen pen)
+    {
+        if (id != _lastPenID) return;
+        ResetVisuals();
+        pen.PlayButtonUpClip();
+    }
+
+    //public void OnHoverEnter(int id) { }
+    //public void OnHoverExit(int id)
+    //{
+    //    if (id == _lastPenID) OnUp(id);
+    //}
+
     public void OnButtonChange()
     {
-        OnButtonUp();
+        // When the page changes, we stop events, but we ONLY move the mesh UP
+        // if the pen is no longer holding this button.
+        updateToggleText();
+        if (_lastPenID == -1)
+        {
+            ResetVisuals();
+        }
+        else
+        {
+            // Stop continuous events even if held
+            if (MFDManager != null && MFDManager.ContinousEventScript == ContinousEventScript)
+            {
+                MFDManager.ContinousEventScript = null;
+                MFDManager.ContinousEventName = null;
+            }
+        }
+    }
+
+    private void ResetVisuals()
+    {
+        InitializePosition();
+
+        // Move mesh UP
+        if (buttonMesh != null)
+            buttonMesh.localPosition = _startLocalPos;
+
+        if (MFDManager != null && MFDManager.ContinousEventScript == ContinousEventScript)
+        {
+            MFDManager.ContinousEventScript = null;
+            MFDManager.ContinousEventName = null;
+        }
+
+        _lastPenID = -1;
     }
 
     public void ChangePage()
     {
-        if (NextPage == null)
+        if (NextPage != null) MFDManager.ChangePage(NextPage);
+
+    }
+
+    public void updateToggleText()
+    {
+        if(EventScript == null || boolVariableName == "")
         {
             return;
         }
-        MFDManager.ChangePage(NextPage);
-    }
+        bool state = (bool)EventScript.GetProgramVariable(boolVariableName);
 
+        textToggleColor.text = state ? "ON" : "OFF";
+        textToggleColor.color = state ? Color.green : Color.red;
+    }
 }
---
+++ Assets/Spiritmarsrover/CustomUI/Tablet/TabletPen.cs
@@ -0,0 +1,220 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class TabletPen : UdonSharpBehaviour
+{
+    public float rayDistance = 0.05f;
+    public LayerMask interactionLayers; // Ensure this includes the MFD Button layer!
+
+    private bool _wasTouching;
+    private TabletButton _activeTabletBtn;
+    private MFDButton _activeMFDBtn;
+
+    private TabletButton _lastHoveredTabletBtn;
+    private MFDButton _lastHoveredMFDBtn;
+
+    public int penID;
+    public bool isRightHand;
+    private string _triggerAxis;
+    public float desktopRayDistance = 1.0f;
+
+    public GameObject PenMesh;
+    public AudioSource ButtonAudioSource;
+    public AudioClip[] ButtonUpClip;
+    public AudioClip[] ButtonDownClip;
+
+    void Start()
+    {
+        _triggerAxis = isRightHand ? "Oculus_CrossPlatform_SecondaryIndexTrigger" : "Oculus_CrossPlatform_PrimaryIndexTrigger";
+        ButtonAudioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (!Networking.LocalPlayer.IsUserInVR() && !isRightHand)
+        {
+            return;
+        }
+        bool triggerHeld = (Input.GetAxisRaw(_triggerAxis) > 0.9f) || Input.GetMouseButton(0);
+        RaycastHit hit;
+        bool currentlyHitting = false;
+
+        if (triggerHeld)
+        {
+            Vector3 rayOrigin;
+            Vector3 rayDirection;
+            float dist;
+
+            if (!Networking.LocalPlayer.IsUserInVR())
+            {
+                // Get Camera position and rotation from TrackingData
+                VRCPlayerApi.TrackingData headData = Networking.LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head);
+                rayOrigin = headData.position;
+                rayDirection = headData.rotation * Vector3.forward;
+                dist = desktopRayDistance;
+            }
+            else
+            {
+                // VR Pen logic
+                rayOrigin = transform.position;
+                rayDirection = -transform.up;
+                dist = rayDistance;
+            }
+
+            //currentlyHitting = Physics.Raycast(rayOrigin, rayDirection, out hit, dist, interactionLayers);
+            currentlyHitting = Physics.Raycast(rayOrigin, rayDirection, out hit, dist, interactionLayers, QueryTriggerInteraction.Ignore);
+
+
+            if (currentlyHitting)
+            {
+                // 1. Check for Tablet (UI Logic)
+                TabletScreen screen = hit.collider.GetComponent<TabletScreen>();
+                if (screen != null)
+                {
+                    HandleTabletInteraction(screen.GetButtonAtPoint(hit.point));
+                }
+                // 2. Check for MFD (Physical Logic)
+                else
+                {
+                    MFDButton mfdBtn = hit.collider.GetComponent<MFDButton>();
+                    HandleMFDInteraction(mfdBtn);
+                }
+            }
+        }
+
+        // Release logic
+        if (!currentlyHitting && _wasTouching)
+        {
+            ClearAllInteractions();
+        }
+
+        _wasTouching = currentlyHitting;
+    }
+
+    private void HandleTabletInteraction(TabletButton hovered)
+    {
+        if (hovered != _lastHoveredTabletBtn)
+        {
+            if (_lastHoveredTabletBtn != null) _lastHoveredTabletBtn.OnHoverExit(penID);
+            if (hovered != null) hovered.OnHoverEnter(penID);
+            _lastHoveredTabletBtn = hovered;
+        }
+
+        if (!_wasTouching)
+        {
+            _activeTabletBtn = hovered;
+            if (_activeTabletBtn != null) _activeTabletBtn.OnDown(penID);
+        }
+        else if (_activeTabletBtn != null)
+        {
+            _activeTabletBtn.OnStay(penID);
+        }
+    }
+
+    private void HandleMFDInteraction(MFDButton hovered)
+    {
+        if (hovered != _lastHoveredMFDBtn)
+        {
+            //if (_lastHoveredMFDBtn != null) _lastHoveredMFDBtn.OnHoverExit(penID);
+            //if (hovered != null) hovered.OnHoverEnter(penID);
+            _lastHoveredMFDBtn = hovered;
+        }
+
+        if (!_wasTouching)
+        {
+            _activeMFDBtn = hovered;
+            if (_activeMFDBtn != null)
+            {
+                _activeMFDBtn.OnDown(penID,this);
+                //ButtonAudioSource.pitch = Random.Range(.95f, 1.05f);
+                //ButtonAudioSource.volume = Random.Range(.9f, 1f);
+                //ButtonAudioSource.PlayOneShot(ButtonDownClip[Random.Range(0, ButtonDownClip.Length)]);
+            }
+        }
+        else if (_activeMFDBtn != null)
+        {
+            _activeMFDBtn.OnStay(penID);
+        }
+    }
+
+    private void ClearAllInteractions()
+    {
+        if (_activeTabletBtn != null)
+        {
+            if (_activeTabletBtn == _lastHoveredTabletBtn)
+            {
+                _activeTabletBtn.OnUp(penID);
+                //ButtonAudioSource.pitch = Random.Range(.95f, 1.05f);
+                //ButtonAudioSource.volume = Random.Range(.9f, 1f);
+                //ButtonAudioSource.PlayOneShot(ButtonUpClip[Random.Range(0, ButtonUpClip.Length)]);
+            }
+            else _activeTabletBtn.OnHoverExit(penID);
+            _activeTabletBtn = null;
+        }
+        if (_activeMFDBtn != null)
+        {
+            if (_activeMFDBtn == _lastHoveredMFDBtn)
+            {
+                _activeMFDBtn.OnUp(penID,this);
+                //ButtonAudioSource.pitch = Random.Range(.95f, 1.05f);
+                //ButtonAudioSource.volume = Random.Range(.9f, 1f);
+                //ButtonAudioSource.PlayOneShot(ButtonUpClip[Random.Range(0, ButtonUpClip.Length)]);
+            }
+            //else _activeMFDBtn.OnHoverExit(penID);
+            _activeMFDBtn = null;
+        }
+
+        if (_lastHoveredTabletBtn != null) { _lastHoveredTabletBtn.OnHoverExit(penID); _lastHoveredTabletBtn = null; }
+       // if (_lastHoveredMFDBtn != null) { _lastHoveredMFDBtn.OnHoverExit(penID); _lastHoveredMFDBtn = null; }
+    }
+
+    private int _zoneCount = 0;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        //Prevent Desktoppers pen mesh from enabling
+        if (!Networking.LocalPlayer.IsUserInVR())
+        {
+            return;
+        }
+        if (other.name == "PenEnable")
+        {
+            _zoneCount++;
+            PenMesh.SetActive(true);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        //Prevent Desktoppers pen mesh from enabling
+        if (!Networking.LocalPlayer.IsUserInVR())
+        {
+            return;
+        }
+        if (other.name == "PenEnable")
+        {
+            _zoneCount--;
+            // Only turn off if we aren't inside ANY valid zones
+            if (_zoneCount <= 0)
+            {
+                _zoneCount = 0; // Safety reset
+                PenMesh.SetActive(false);
+            }
+        }
+    }
+    public void PlayButtonUpClip()
+    {
+        ButtonAudioSource.pitch = Random.Range(.9f, 1.1f);
+        ButtonAudioSource.volume = Random.Range(.9f, 1f);
+        ButtonAudioSource.PlayOneShot(ButtonUpClip[Random.Range(0, ButtonUpClip.Length)]);
+    }
+    public void PlayButtonDownClip()
+    {
+        ButtonAudioSource.pitch = Random.Range(.9f, 1.1f);
+        ButtonAudioSource.volume = Random.Range(.9f, 1f);
+        ButtonAudioSource.PlayOneShot(ButtonDownClip[Random.Range(0, ButtonDownClip.Length)]);
+    }
+}
