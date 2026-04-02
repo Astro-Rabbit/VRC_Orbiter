405126f6f862429a21d75184f7f86a702562a908
DESKTOP-P2AGUDS\Titan <d3coder72@gmail.com>
2026-04-02 16:24:32 -0500

    Lobby Orrery Control remote prefab.

---
+++ Assets/Scripts/OrbiterSim/Orrery/OrreryVisibilityToggle.cs
@@ -0,0 +1,52 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class OrreryVisibilityToggle : UdonSharpBehaviour
+{
+    public GameObject Orrery;
+    public byte OrreryState = 1;
+    public GameObject OrreryRemote;
+    public GameObject OrreryRemoteRespawn;
+    public VRC_Pickup pickup;
+    void Start()
+    {
+        
+    }
+    public void OrreryOn()
+    {
+        Orrery.SetActive(true);
+    }
+    public void OrreryOff()
+    {
+        Orrery.SetActive(false);
+    }
+    public void OrreryToggle()
+    {
+        if(OrreryState == 1)
+        {
+            Orrery.SetActive(true);
+        }
+        else
+        {
+            Orrery.SetActive(false);
+        }
+        
+    }
+    public void RecallRemote()
+    {
+        Networking.SetOwner(Networking.LocalPlayer, OrreryRemote);
+        OrreryRemote.transform.position = OrreryRemoteRespawn.transform.position;
+        OrreryRemote.transform.rotation = OrreryRemoteRespawn.transform.rotation;
+
+    }
+    public override void OnOwnershipTransferred(VRCPlayerApi player)
+    {
+        if (pickup.IsHeld)
+        {
+            pickup.Drop();
+        }
+    }
+}
