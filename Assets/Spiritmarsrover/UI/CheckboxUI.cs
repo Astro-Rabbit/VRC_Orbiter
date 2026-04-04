b257b93196b2653d47f0bc725b444bd094070446
DESKTOP-P2AGUDS\Titan <d3coder72@gmail.com>
2026-04-04 16:57:54 -0500

    Finishing personal setting. Just need to do skybox settings.

---
+++ Assets/Spiritmarsrover/UI/CheckboxUI.cs
@@ -0,0 +1,38 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.UI;
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class CheckboxUI : UdonSharpBehaviour
+{
+    //public GameObject checkmark;
+    public Image panel;
+    public bool isOn = false;
+    public GameObject toggleObject;
+    void Start()
+    {
+        updateVisual();
+    }
+    public void ToggleUI()
+    {
+        isOn = !isOn;
+        updateVisual();
+
+
+    }
+    public void updateVisual()
+    {
+        //checkmark.SetActive(isOn);
+        if (isOn)
+        {
+            panel.color = Color.green;
+        }
+        else
+        {
+            panel.color = Color.red;
+        }
+        toggleObject.SetActive(isOn);
+    }
+}
